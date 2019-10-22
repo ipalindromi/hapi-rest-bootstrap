5dab557f3a3e1cdae576fd0db163440dab33bb0a
Dustin Baldwin <ipalindromi@gmail.com>
2019-10-22 18:20:29 -0500

    Added test of method importing.

--- src/__tests__/server-helpers.spec.js
+++ src/__tests__/server-helpers.spec.js
@@ -3,24 +3,28 @@ const path = require('path');
 
 jest.mock('fs');
 
-const { findRoutes } = require('../server-helpers');
+const { findMethods, findRoutes } = require('../server-helpers');
 
 beforeEach(() => {
 	jest.resetModules();
 });
 
 describe('findMethods', () => {
-	test('It will find routes in the file system', () => {
-		// These are used to find routes
-		fs.readdirSync.mockReturnValue(['example.js']);
+	beforeEach(() => {
 		fs.statSync.mockReturnValue({
 			isDirectory: () => false,
 		});
+	});
+
+	test('It will findRoutes in the file system', () => {
+		// These are used to find routes
+		fs.readdirSync.mockReturnValue(['example.js']);
 
 		const testFunc = () => 'test';
 
 		// The `virtual` is important here, and makes it possible to test
 		// for a module that does not exist. We provide our own implementation.
+		// doMock is used instead of `mock` to avoid premature hoisting.
 		jest.doMock(
 			path.join(__dirname, '..', '/routes/example.js'),
 			() => ({
@@ -31,5 +35,23 @@ describe('findMethods', () => {
 
 		const routes = findRoutes();
 		expect(routes).toStrictEqual([testFunc]);
+	});
+
+	test('It will findMethods in the file system', () => {
+		// These are used to find routes
+		fs.readdirSync.mockReturnValue(['example.js']);
+
+		const testFunc = () => 'test';
+
+		jest.doMock(
+			path.join(__dirname, '..', '/methods/example.js'),
+			() => ({
+				testFunc,
+			}),
+			{ virtual: true },
+		);
+
+		const routes = findMethods();
+		expect(routes).toStrictEqual([['testFunc', testFunc]]);
 	});
 });
